a6068e8259dd070921dbc4a4068b9a625a9019ae
NatMin143 <109016941+NatMin143@users.noreply.github.com>
2025-08-20 23:40:23 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  name: 'Nathaniel B. Ministros',
+  title: 'Ministros HCI - Login Form',
+  description: 'Created with the help of v0',
 }
 
 export default function RootLayout({
